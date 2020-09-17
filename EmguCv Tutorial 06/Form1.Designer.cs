a0f7fc550fc13599e91557d353a3214e4ae39293
Salinda <salinda.jayasundara3010@gmail,com>
2020-09-17 10:38:38 +0530

    final commit

---
+++ EmguCv Tutorial 06/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace EmguCv_Tutorial_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            this.histogramBox2 = new Emgu.CV.UI.HistogramBox();
+            this.histogramBox3 = new Emgu.CV.UI.HistogramBox();
+            this.Button01 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(445, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "EmguCv Tutorial 06";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackColor = System.Drawing.Color.White;
+            this.imageBox1.Location = new System.Drawing.Point(159, 101);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(407, 308);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BackColor = System.Drawing.Color.White;
+            this.imageBox2.Location = new System.Drawing.Point(597, 101);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(395, 308);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BackColor = System.Drawing.Color.White;
+            this.imageBox3.Location = new System.Drawing.Point(1023, 101);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(394, 308);
+            this.imageBox3.TabIndex = 4;
+            this.imageBox3.TabStop = false;
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.histogramBox1.Location = new System.Drawing.Point(159, 431);
+            this.histogramBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(407, 278);
+            this.histogramBox1.TabIndex = 5;
+            // 
+            // histogramBox2
+            // 
+            this.histogramBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.histogramBox2.Location = new System.Drawing.Point(597, 431);
+            this.histogramBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.histogramBox2.Name = "histogramBox2";
+            this.histogramBox2.Size = new System.Drawing.Size(395, 278);
+            this.histogramBox2.TabIndex = 6;
+            // 
+            // histogramBox3
+            // 
+            this.histogramBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.histogramBox3.Location = new System.Drawing.Point(1023, 431);
+            this.histogramBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.histogramBox3.Name = "histogramBox3";
+            this.histogramBox3.Size = new System.Drawing.Size(394, 278);
+            this.histogramBox3.TabIndex = 7;
+            // 
+            // Button01
+            // 
+            this.Button01.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Button01.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button01.Location = new System.Drawing.Point(31, 205);
+            this.Button01.Name = "Button01";
+            this.Button01.Size = new System.Drawing.Size(102, 47);
+            this.Button01.TabIndex = 8;
+            this.Button01.Text = "Open";
+            this.Button01.UseVisualStyleBackColor = false;
+            this.Button01.Click += new System.EventHandler(this.Button01_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(31, 529);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 43);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Histogram";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1473, 722);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Button01);
+            this.Controls.Add(this.histogramBox3);
+            this.Controls.Add(this.histogramBox2);
+            this.Controls.Add(this.histogramBox1);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox2;
+        private Emgu.CV.UI.HistogramBox histogramBox3;
+        private System.Windows.Forms.Button Button01;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EmguCv Tutorial 06/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace EmguCv_Tutorial_06
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, Byte> InputImage;
+        Image<Gray, Byte> R;
+        Image<Gray, Byte> G;
+        Image<Gray, Byte> B;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button01_Click(object sender, EventArgs e)
+        {
+            String FileName = "D:\\Visual Studio 2019 workspace\\6a.png";
+            InputImage = new Image<Bgr, byte>(FileName);
+
+            B = InputImage[0];
+            G = InputImage[1];
+            R = InputImage[2];
+
+            imageBox1.Image = R;
+            imageBox2.Image = G;
+            imageBox3.Image = B;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (InputImage == null)
+            {
+                MessageBox.Show("Please select an Image");
+                return;
+            }
+
+            histogramBox1.ClearHistogram();
+            histogramBox2.ClearHistogram();
+            histogramBox3.ClearHistogram();
+
+            histogramBox1.GenerateHistograms(R, 256);
+            histogramBox2.GenerateHistograms(G, 256);
+            histogramBox3.GenerateHistograms(B, 256);
+
+            histogramBox1.Refresh();
+            histogramBox2.Refresh();
+            histogramBox3.Refresh();
+        }
+    }
+}
